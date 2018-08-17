8e52514a8a794366620b23d8bcd90ba1f90bf64a
Lewis <lewis-yinan.lu@cn.abb.com>
2018-08-17 16:59:19 +0800

    add public folder and static midware

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res)=>{
-    res.send('Hello Express!');
+    // res.send('<h1>Hello Express!</h1>');
+    res.send({
+         name:'Mark',
+         likes:['apple', 'chocolate'],
+    });
+});
+
+app.get('/about', (req, res)=>{
+    res.send('About Page');
+});
+
+app.get('/bad', (req, res)=>{
+    res.send({
+        error: 'Something wrong',
+    });
 });
 
 app.listen(3000);
