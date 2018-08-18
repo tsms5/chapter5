4186f45f3a0e95a2f587951fe4a43e574cf5e638
mark_lu <marklureal@gmail.com>
2018-08-18 17:52:46 +0800

    add hbs

--- server.js
+++ server.js
@@ -1,19 +1,54 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+
+app.use((req, res, next)=>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err)=>{
+        if (err){
+            console.log('Unable to append to server.log.')
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next)=>{
+//     res.render('maintenace.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase();
+});
 
 app.get('/', (req, res)=>{
     // res.send('<h1>Hello Express!</h1>');
-    res.send({
-         name:'Mark',
-         likes:['apple', 'chocolate'],
+    res.render('home.hbs',{
+        pageTitle: 'Home Page Dynamic',
+        welcomeMessage: 'Welcome to my home',
     });
 });
 
 app.get('/about', (req, res)=>{
-    res.send('About Page');
+    res.render('about.hbs',{
+        pageTitle: 'About Page Dynamic',
+    })
 });
 
 app.get('/bad', (req, res)=>{
@@ -22,4 +57,6 @@ app.get('/bad', (req, res)=>{
     });
 });
 
-app.listen(3000);
+app.listen(3000, ()=>{
+    console.log("Server is up on port 3000");
+});
